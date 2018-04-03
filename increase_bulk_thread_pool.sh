cb02477abba128a2c8b92fe3435ad600297b52d8
Julian Simioni <julian@simioni.org>
2018-04-03 10:04:39 -0400

    Add thread pool script

---
+++ increase_bulk_thread_pool.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+se -eu
+
+echo "setting threadpool.bulk.queue_size to 500 on $cluster_url"
+curl -XPUT ${cluster_url}/_cluster/settings -d '{ "transient" : { "threadpool.bulk.queue_size" : 500 } }'"A
