84eaffd52387aa3db177d7e552645ad938d85250
Julian Simioni <julian@simioni.org>
2018-04-03 10:34:21 -0400

    Fix extra chars

--- increase_bulk_thread_pool.sh
+++ increase_bulk_thread_pool.sh
@@ -2,4 +2,4 @@
 se -eu
 
 echo "setting threadpool.bulk.queue_size to 500 on $cluster_url"
-curl -XPUT ${cluster_url}/_cluster/settings -d '{ "transient" : { "threadpool.bulk.queue_size" : 500 } }'"A
+curl -XPUT ${cluster_url}/_cluster/settings -d '{ "transient" : { "threadpool.bulk.queue_size" : 500 } }'
