1796b00dcd0ba14beab16e2b4d94a069f493d9ac
Julian Simioni <julian@simioni.org>
2018-04-04 11:54:53 -0400

    Add test script

---
+++ test/test_create_index.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -euo pipefail
+
+curl -XPUT "$cluster_url/twitter?pretty" -H 'Content-Type: application/json' -d'
+{
+    "settings" : {
+        "index" : {
+            "number_of_shards" : 3,
+            "number_of_replicas" : 2
+        }
+    }
+}
+'
+
+curl -XDELETE "$cluster_url/twitter"
