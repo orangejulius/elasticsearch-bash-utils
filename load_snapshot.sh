c7265ba150c420750e84c7ed8461b81d3a811744
Julian Simioni <julian@simioni.org>
2018-04-04 11:59:33 -0400

    Add load snapshot script

---
+++ load_snapshot.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -ex
+
+##
+# loads a snapshot onto a dev cluster
+##
+
+set -euo pipefail
+
+base_path=${base_path:-elasticsearch}
+es_repo_name="pelias_snapshots"
+s3_bucket="${s3_bucket:-geocodeearth-mapzen-elasticsearch-snapshots}"
+new_snapshot_name="${new_snapshot_name:-pelias-2017.11.18-001123}"
+
+# create bucket, only needs to be run once
+curl -XPOST "$cluster_url/_snapshot/$es_repo_name" -d "{
+ \"type\": \"s3\",
+   \"settings\": {
+   \"bucket\": \"$s3_bucket\",
+   \"base_path\" : \"$base_path\",
+   \"max_snapshot_bytes_per_sec\" : \"1000mb\"
+ }
+}"
+
+## import new snapshot with name including timestamp
+curl -XPOST "$cluster_url/_snapshot/$es_repo_name/${new_snapshot_name}/_restore" -d "{
+  \"indices\": \"pelias\",
+  \"rename_pattern\": \"pelias\",
+  \"rename_replacement\": \"$new_snapshot_name\"
+}"
