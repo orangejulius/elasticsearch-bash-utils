3ccac7e0a866cad1a2139e7a7848758fc1dea1a4
Julian Simioni <julian@simioni.org>
2018-04-09 15:29:37 -0400

    Fix make_alias script

--- make_alias.sh
+++ make_alias.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
-echo "setting pelias alias to $index_name on $cluster_url
+index_name="${index_name:-pelias-2017.11.18-001123}"
 
+echo "setting pelias alias to $index_name on $cluster_url"
 
 curl -XPOST "$cluster_url/_aliases" -d "{
   \"actions\": [{
