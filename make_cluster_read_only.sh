881eba1c930a1769b2171b96c4028fdbf5f8f355
Julian Simioni <julian@simioni.org>
2019-03-14 16:49:03 -0400

    Add scripts to control cluster read/write status

---
+++ make_cluster_read_only.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -ue
+
+set -ue
+
+cluster_url="${cluster_url:-http://localhost:9200}"
+
+curl -s -XPUT "$cluster_url/_cluster/settings" -d '{
+  "persistent" : {
+    "cluster.blocks.read_only" : true
+  }
+}'
