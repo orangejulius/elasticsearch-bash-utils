d1a326b9cb724a4cdd03a39c9ea0e56f4b320851
Julian Simioni <julian@simioni.org>
2019-05-14 09:31:34 -0400

    Add disk watermark script

---
+++ update_watermarks.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euo pipefail
+
+cluster_url="${cluster_url:-http://localhost:9200}"
+echo "setting disk watermark levels for $cluster_url"
+
+curl -XPUT "$cluster_url/_cluster/settings" -d '{
+  "persistent": {
+    "cluster.routing.allocation.disk.watermark.low": "95%",
+    "cluster.routing.allocation.disk.watermark.high": "95%",
+  }
+}'
