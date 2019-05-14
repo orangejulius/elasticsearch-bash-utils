727628a5807f3b79e66645d83281842bf3e9fa40
Julian Simioni <julian@simioni.org>
2019-05-14 09:32:28 -0400

    Remove extra comma

--- update_watermarks.sh
+++ update_watermarks.sh
@@ -7,6 +7,6 @@ echo "setting disk watermark levels for $cluster_url"
 curl -XPUT "$cluster_url/_cluster/settings" -d '{
   "persistent": {
     "cluster.routing.allocation.disk.watermark.low": "95%",
-    "cluster.routing.allocation.disk.watermark.high": "95%",
+    "cluster.routing.allocation.disk.watermark.high": "95%"
   }
 }'
